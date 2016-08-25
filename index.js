274a9a54b897e56453850117457aded1a3ec69b0
Mikael Lindahl <mikael.lindahl@greencargo.com>
2016-08-25 14:17:25 +0200

    added new functionallity

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 /**
  * Created by Mikael Lindahl on 2016-07-19.
  */
-
+'use strict';
 
 /**
  * Options:
  *
  *   - `label` label for bar to show
  *   - `total` total number of ticks to complete
+ *   - 'draw' "bar", "percentage" or your own draw function
  *   - 'append' If true show accumulated tick text separated with cmoma
- *   - `time` show elapsed time (boolean)
- *   
+ *   - `show` what to show ['bar', 'percent', 'count','time'] (list)
+ *
  *   @param {object} options
  *   @api public
  */
@@ -18,13 +19,42 @@
 function Bar(options) {
     this.label = options.label || 'Processing';
     this.total = options.total;
+    this.draw = options.draw || 'single';
     this.counter = 0;
     this.append = options.append;
     this.complete = false;
-    this.time=options.time || false;
-    this.timer=undefined;
+    this.show = this.show || {
+            overwrite: true,
+            label:{
+                color:'\x1b[1;37m', // bold
+            },
+            bar: {
+                length: 10,
+                color: '\x1b[0;37m', // white
+                completed:'=',
+                incompleted:'_'
+            },
+            percent: {
+                color: '\x1b[1;36m', //cyan bold
+            },
+            count: {
+                color: '\x1b[1;35m' //magenta bold
+            },
+            time: {
+                color: '\x1b[1;33m' // green bold
+            }
+        };
+    this.timer = undefined;
+    this.stream = options.stream || process.stdout;
 }
 
+let _colors={
+    green_bold:'\x1b[1;33m',
+    magenta_bold:'\x1b[1;35m',
+    cyan_bold :'\x1b[1;36m',
+    bold:'\x1b[1;37m',
+    normal:'\x1b[0;37m',
+};
 
 /**
  * "tick" the progress bar with optional `text` shown at tick.
@@ -34,7 +64,7 @@ function Bar(options) {
  */
 Bar.prototype.tick = function (text) {
 
-    if(this.counter==0  && this.time){
+    if(this.counter==0  && this.show.time){
         this.timer=new Date().valueOf();
     }
 
@@ -50,28 +80,110 @@ Bar.prototype.tick = function (text) {
         this.text = text;
     }
 
-    var green_bold = '\x1b[1;33m';
-    var magenta_bold = '\x1b[1;35m';
-    var cyan_bold = '\x1b[1;36m';
-    var bold = '\x1b[1;37m';
-    var normal = '\x1b[0;37m';
+    this._draw();
 
-    process.stdout.write('\r' + bold + this.label + ': ');
-    process.stdout.write(cyan_bold + '[' +
-        ('  ' + Math.round(100 * this.counter / (this.total))).slice(-3) + ' %] ');
-    process.stdout.write(magenta_bold + '[' +
-        ('   ' + this.counter + '(' + this.total + ')').slice(-String(this.total).length * 4) + '] ')
-    if (this.time) {
-        var val=Math.round((new Date().valueOf()-this.timer)/1000);
-        process.stdout.write(green_bold + '[' +val + ' sec] ')
-    }
-    process.stdout.write(normal + this.text);
+
 
     if (this.counter == this.total) {
-        this.complete=true
+        this.complete=true;
         console.log(' '); //insert rowbreak
     };
 };
 
+Bar.prototype._draw=function(){
 
-module.exports = Bar;
+    let count='     ' + this.counter + '(' + this.total + ')';
+    let info='';
+
+    if (this.show.bar) {
+        info = _colors.cyan_bold + '[' + ('  ' + Math.round(100 * this.counter / (this.total))).slice(-3) + ' %] ';
+    }
+
+    if (this.show.count) {
+        info += _colors.magenta_bold + '[' + (count).slice(count.length - (String(this.total).length * 2 + 2), count.length) + '] ';
+    }
+
+    if (this.show.time) {
+        var val = Math.round((new Date().valueOf() - this.timer) / 1000);
+        info+=_colors.green_bold + '[' + val + ' sec] ';
+    }
+
+    if (this.show.overwrite) {
+
+        let str='\r'+this.show.label.color + this.label + ': ';
+
+        if (this.show.bar){
+            str+=this.show.bar.color +'[';
+            let n_dots= Math.round(this.show.bar.length*(this.counter / this.total));
+
+            for (let i = 0; i <n_dots; i++) {
+                str += this.show.bar.completed || '.'
+            };
+
+            for (let i = 0; i <this.show.bar.length-n_dots; i++) {
+                str += this.show.bar.incompleted || ' '
+            };
+
+            str+='] ';
+        };
+
+        str+=info;
+
+        str+=_colors.normal + this.text;
+        // console.log(str)
+        this.stream.write(str)
+
+    }
+
+    if ( !this.show.overwrite ) {
+
+        let n = 10;
+        let str = '';
+
+        if (this.counter - 1 == 0) {
+
+            str += _colors.bold + this.label + '\n[.';
+            this.stream.write(str);
+            return
+        }
+
+        if (this.show.bar){
+
+            if (this.counter == this.total) {
+
+                let space = '';
+                if(this.total>this.show.bar.length)
+                for (let i = 0; i < n - 1 - ((this.counter - 1) % n); i++) {
+                    space += ' '
+                }
+
+                if ((this.counter % n) == 1) {
+                    str += _colors.normal + '['
+                }
+                str += '.]' + space
+
+            } else if (((this.counter - 1) % n) == 0) {
+
+                str += _colors.normal + '[.'
+
+            }
+            else if (((this.counter - 1) % n) == n - 1) {
+
+                str += '.]';
+
+            } else {
+
+                str += '.';
+            }
+        }
+        if(this.counter == this.total || (((this.counter - 1) % n) == n - 1)){
+            str+=info+'\n'
+        }
+        this.stream.write(str)
+
+    }
+};
+
+
+
+module.exports = (options)=> {return new Bar(options)};
--- test/index.js
+++ test/index.js
@@ -6,6 +6,7 @@
 
 const Code = require('code');   // assertion library
 const Lab=require('lab');
+const Fs = require('fs');
 const Bar = require('../index.js');
 
 var lab = exports.lab = Lab.script();
@@ -16,62 +17,85 @@ lab.experiment('bar', function () {
         done();
     });
 
-    lab.test('tick', function (done) {
-
-        var options = {
-            label: 'Progress bar',
-            total: 30,
-            time:true,
-        };
-
-
-        var bar = new Bar(options);
-        var i=1;
-        var timer = setInterval(function () {
-            bar.tick('Tick number '+i);
-            if (bar.complete) {
-                clearInterval(timer);
-                done();
-            }
-            i++;
-        }, 100);
-    });
+    // lab.test('tick', function (done) {
+    //
+    //     var options = {
+    //         label: 'Progress bar',
+    //         total: 30,
+    //         time: true,
+    //     };
+    //
+    //     var bar = new Bar(options);
+    //     var i = 1;
+    //     var timer = setInterval(function () {
+    //         bar.tick('Tick number ' + i);
+    //         if (bar.complete) {
+    //             clearInterval(timer);
+    //             done();
+    //         }
+    //         i++;
+    //     }, 100);
+    // });
 
     lab.test('two in a row', function (done) {
 
-        var options = {
+        let options = {
             label: 'First progress bar',
             total: 5
         };
 
-        var bar = new Bar(options);
-        var i=1;
-        var timer = setInterval(function () {
-            bar.tick('Tick number '+i);
-            if (bar.complete) {
-                clearInterval(timer);
+        let bar = Bar(options);
 
-                options.label='Second progress bar'
+        let timer= (options, callback) => {
 
-                bar = new Bar(options);
-                var j=1;
-                timer = setInterval(function () {
-                    bar.tick('Tick number '+j);
-                    if (bar.complete) {
-                        clearInterval(timer);
+            let i = 1;
 
+            setInterval(function () {
+                bar.tick('Tick number ' + i);
+                if (bar.complete) {
+                    clearInterval(timer);
 
-                        done();
-                    }
-                    j++;
-                }, 100);
+                    options.label = 'Second progress bar';
+                    bar.reset().total(10);
 
-            }
-            i++;
-        }, 100);
+                    if (callback){callback(options);}
 
-
+                }
+                i++;
+            }, 100);
+        };
+        timer(options, timer)
 
     });
+
+
+    // lab.test('file stream', function (done) {
+    //
+    //     console.log(__dirname+'/test/test.txt')
+    //     var options = {
+    //         label: 'Progress bar',
+    //         total: 21,
+    //         time:true,
+    //         // stream: Fs.createWriteStream(
+    //         //     __dirname+'/test.txt', {
+    //         //     flags: 'w+',
+    //         //     defaultEncoding: 'ascii'
+    //         //     }
+    //         // )
+    //         draw:'single',
+    //     };
+    //
+    //
+    //     var bar = new Bar(options);
+    //     var i=1;
+    //     var timer = setInterval(function () {
+    //         bar.tick('Tick number '+i);
+    //         if (bar.complete) {
+    //             clearInterval(timer);
+    //             done();
+    //         }
+    //         i++;
+    //     }, 100);
+    // });
 
 });
